9d8c91c40b6acff9b4c0253e6cc186481b5ec7fe
LLeo93 <liba.leoncini@gmail.com>
2026-05-21 17:41:21 +0200

    miglioramenti SEO

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -9,15 +9,26 @@ interface SeoProps {
 }
 
 const Seo: React.FC<SeoProps> = ({ title, description, image, url }) => {
+  const baseUrl = 'https://portfolio-psi-lilac-74.vercel.app';
+
+  const fullUrl = url ? `${baseUrl}${url}` : baseUrl;
+
   return (
     <Helmet>
       <title>{title}</title>
+
       {description && <meta name="description" content={description} />}
-      {url && <link rel="canonical" href={url} />}
-      {image && <meta property="og:image" content={image} />}
+
+      {url && <link rel="canonical" href={fullUrl} />}
+
       <meta property="og:title" content={title} />
+
       {description && <meta property="og:description" content={description} />}
-      {url && <meta property="og:url" content={url} />}
+
+      {url && <meta property="og:url" content={fullUrl} />}
+
+      {image && <meta property="og:image" content={image} />}
+
       <meta property="og:type" content="website" />
     </Helmet>
   );
