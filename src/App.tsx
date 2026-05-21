e54a756c5a678cd74cf5a3b960fb39a5c3241bf4
LLeo93 <liba.leoncini@gmail.com>
2026-05-21 13:18:57 +0200

    test5

--- src/App.tsx
+++ src/App.tsx
@@ -27,15 +27,12 @@ function AnimatedRoutes() {
         key={location.pathname}
         initial={{
           opacity: 0,
-          y: 12,
         }}
         animate={{
           opacity: 1,
-          y: 0,
         }}
         exit={{
           opacity: 0,
-          y: -8,
         }}
         transition={{
           duration: 0.45,
