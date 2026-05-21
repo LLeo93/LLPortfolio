34b2c731d620d7052565317751e21557f3f0c0a9
LLeo93 <liba.leoncini@gmail.com>
2026-05-21 13:34:55 +0200

    test

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -40,7 +40,7 @@ const MainContent: React.FC<MainContentProps> = ({ children }) => {
         bg-black bg-opacity-40 text-gray-200 shadow-lg z-50 p-6
         flex flex-col rounded-3xl w-full max-w-sm sm:max-w-md my-6
         md:max-w-xl md:w-3/4
-        lg:relative lg:top-8 lg:right-6 lg:bottom-6 lg:w-[calc(100vw-450px)] lg:max-w-none lg:items-center lg:mx-0 lg:my-0
+        lg:fixed lg:top-8 lg:right-6 lg:bottom-6 lg:w-[calc(100vw-450px)] lg:max-w-none lg:items-center lg:mx-0 lg:my-0
        overflow-visible lg:overflow-hidden
       "
     >
